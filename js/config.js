bd883708a3ea78c854e5f04101f56922caf389db
xzhbxjbaxj <120939476+xzhbxjbaxj@users.noreply.github.com>
2025-05-09 16:11:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -26,6 +26,7 @@ const API_SITES = {
         name: '黑木耳',
         detail: 'https://heimuer.tv',
     },
+    
     ffzy: {
         api: 'http://ffzy5.tv',
         name: '非凡影视',
@@ -38,7 +39,7 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
+        adult: fasle
     },
     zy360: {
         api: 'https://360zy.com',
@@ -80,32 +81,32 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
+        adult: false
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
+        adult: false
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
+        adult: false
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
+        adult: false
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
+        adult: false
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
+        adult: false,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -115,10 +116,10 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
+        adult: false
     },
     yinghua: {
-        api: 'https://m3u8.apiyhzy.com',
+        api: 'https://yhzy.cc',
         name: '樱花资源'
     },
     baidu: {
@@ -209,4 +210,4 @@ const CUSTOM_API_CONFIG = {
 };
 
 // 新增隐藏内置黄色采集站API的变量，默认为true
-const HIDE_BUILTIN_ADULT_APIS = true;
+const HIDE_BUILTIN_ADULT_APIS = false;
