88e0b607002b44b2060797de270ceb04869c4fe2
xzhbxjbaxj <120939476+xzhbxjbaxj@users.noreply.github.com>
2025-05-09 16:14:36 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -26,7 +26,6 @@ const API_SITES = {
         name: '黑木耳',
         detail: 'https://heimuer.tv',
     },
-    
     ffzy: {
         api: 'http://ffzy5.tv',
         name: '非凡影视',
@@ -39,7 +38,7 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: fasle
+        adult: true
     },
     zy360: {
         api: 'https://360zy.com',
@@ -81,32 +80,32 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: false
+        adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: false
+        adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: false
+        adult: true
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: false
+        adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: false
+        adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: false,
+        adult: true,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -119,7 +118,7 @@ const API_SITES = {
         adult: false
     },
     yinghua: {
-        api: 'https://yhzy.cc',
+        api: 'https://m3u8.apiyhzy.com',
         name: '樱花资源'
     },
     baidu: {
