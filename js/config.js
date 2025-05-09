e123539cdac77c6f3120bf7aa6a612d1e3bb7d55
xzhbxjbaxj <120939476+xzhbxjbaxj@users.noreply.github.com>
2025-05-09 16:22:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -38,7 +38,7 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
+        adult: false
     },
     zy360: {
         api: 'https://360zy.com',
@@ -80,32 +80,32 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
+        adult: false
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
+        adult: false
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
+        adult: false
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
+        adult: false
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
+        adult: false
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
+        adult: false,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -115,7 +115,7 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
+        adult: false
     },
     yinghua: {
         api: 'https://m3u8.apiyhzy.com',
@@ -209,4 +209,4 @@ const CUSTOM_API_CONFIG = {
 };
 
 // 新增隐藏内置黄色采集站API的变量，默认为true
-const HIDE_BUILTIN_ADULT_APIS = true;
+const HIDE_BUILTIN_ADULT_APIS =false;
